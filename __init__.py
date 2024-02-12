91f9907260bebf5a212fec727d6073cb16f8038c
DamienSasso <55143216+DamienSasso@users.noreply.github.com>
2024-02-12 08:49:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
+from flask import jsonify
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
@@ -11,9 +12,23 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html') #comm
   
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>" 
+
+@app.route('/paris/')
+def meteo():
+    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+  
 if __name__ == "__main__":
   app.run(debug=True)
   
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+
