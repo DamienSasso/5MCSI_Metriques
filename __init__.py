8768794cdf1554959edd44284ad67fe4e6e635ad
DamienSasso <55143216+DamienSasso@users.noreply.github.com>
2024-02-12 10:12:49 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,18 @@ def mongraphique():
 
 @app.route("/histogramme/")
 def histogramme():
-    return render_template("histogramme.html")
+    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('temp', {}).get('day') - 273.15  # Conversion de Kelvin en °C
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    
+    print(results)  # Imprimez les données récupérées
+    
+    return render_template("histogramme.html", results=results)
 
 
 if __name__ == "__main__":
